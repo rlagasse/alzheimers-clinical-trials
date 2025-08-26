ec7d2da1bf82d34eadebe515e2c26ec6ff29a4a2
rlagasse <rlagasse@sfu.ca>
2025-08-25 20:52:17 -0700

    generalized schema check function to allow inter-schema table comparison of nct_id

--- tests/ptap_validation_tests.sql
+++ tests/ptap_validation_tests.sql
@@ -26,29 +26,26 @@ SELECT set_eq(
 	'Table "ctgov" and Table "alzheimer_subset" do not share the same columns'
 );
 
--- (8) Validate that alzheimer_subset is a subset of ctvgov. Takes primary key as argument
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'conditions');
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'countries');
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'designs');
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'facilities');
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'interventions');
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'outcome_counts');
-SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'sponsors');
--- SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'studies');
+-- (8) Validate that each table in alzheimer_subset is a subset of the table in ctvgov, based on primary key id.
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'conditions', 'conditions', 'id');
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'countries', 'countries', 'id');
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'designs', 'designs', 'id');
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'facilities', 'facilities', 'id');
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'interventions', 'interventions', 'id');
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'outcome_counts', 'outcome_counts', 'id');
+SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'sponsors', 'sponsors', 'id');
+-- SELECT * FROM subset_checker('alzheimer_subset', 'ctgov', 'studies', 'studies', 'id');
 -- Note: studies has no public key as it uses nct_id as per https://aact.ctti-clinicaltrials.org/schema#idInformation
 
 
--- (7) Ensure each record in each table matches an nct_id in studies table
--- SELECT is(
--- 	$$
--- 	SELECT COUNT(*) 
--- 	FROM alzheimer_subset.conditions c
--- 	LEFT JOIN alzheimer_subset.studies s ON c.nct_id = s.nct_id
--- 	WHERE s.nct_id IS NULL $$,
--- 	0::integer,
--- 	'Table "conditions" must have matching "nct_id" records to Table "studies"'
--- );
-
+-- (7) Check each record in each table in alzheimer_subset matches an nct_id in alzheimer_subset's studies table
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'conditions', 'studies', 'nct_id');
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'countries', 'studies', 'nct_id');
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'designs', 'studies', 'nct_id');
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'facilities', 'studies', 'nct_id');
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'interventions', 'studies', 'nct_id');
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'outcome_counts', 'studies', 'nct_id');
+SELECT * FROM subset_checker('alzheimer_subset', 'alzheimer_subset', 'sponsors', 'studies', 'nct_id');
 
 SELECT pass('All tests passed');
 SELECT fail('Not all tests passed');
