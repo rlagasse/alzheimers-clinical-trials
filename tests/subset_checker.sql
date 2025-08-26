02581b5e3d2a1172ae51dff220f75988b4f4e4ff
rlagasse <rlagasse@sfu.ca>
2025-08-25 20:00:10 -0700

    schema check function working

--- tests/subset_checker.sql
+++ tests/subset_checker.sql
@@ -1,33 +1,34 @@
--- Subset checker help function
+-- Subset checker help function. Used on all tables but studies as id column should be the primary key in all.
 CREATE OR REPLACE FUNCTION subset_checker (
     alzheimer_subset text,
     ctgov text,
     table_of_interest text
 )
 
-RETURNS SETOF TEXT AS $$
+-- Return as test message for pass/fail
+RETURNS SETOF TEXT AS 
+$$
 
 DECLARE
-    total_count integer;
+    key_count integer;
 	
 BEGIN
-
+    -- Count how many primary keys that are in alzheimer_subset that aren't in ctgov; should eqal 0
     EXECUTE format(
-        'SELECT COUNT(*) FROM %I.%I EXCEPT SELECT * FROM %I.%I',
-        alzheimer_subset, table_of_interest, ctgov, table_of_interest
-    ) INTO total_count;
+        'SELECT COUNT(*) FROM %I.%I AS keys
+         WHERE keys.id NOT IN (SELECT id FROM %I.%I)',
+        alzheimer_subset, table_of_interest,
+        ctgov, table_of_interest
+    ) INTO key_count;
 
+    -- Return a single pTAP test result
     RETURN QUERY
-
-	-- pTAP function
     SELECT is(
-        total_count::integer,
+        key_count,
         0::integer,
-        format(
-            'Table "%s.%s" is a subset of "%s.%s"',
-            schema1, tablename, schema2, tablename
-        )
+        format('All records (checked by primary keys) in Schema %I Table %I exist in Schema %I Table %I', alzheimer_subset, table_of_interest, ctgov, table_of_interest)
     );
+
 END;
 $$ LANGUAGE plpgsql;
 
